11010cba7eb6abcd3f0c4bcefa405ed476da0591
haykel jabri <haykel.j@gmail.com>
2021-06-15 03:40:20 +0200

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import { Image } from 'antd';
+import { Image, PageHeader } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
 
 const Details = (detailsItem) => {
-
     detailsItem = "https:\/\/fakeimg.pl\/200x320\/?text=Hello";
     return (
+        <div className="detailsSection">
+        <PageHeader className="site-page-header-details"/>
         <div className="detailsContainer">
+            
         <Image
             width={200}
             src={detailsItem}
             />
         </div>
-        
+       </div> 
     );
   
 }
