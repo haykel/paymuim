1bd5f3d731bcc4bbf32452179876d7121d24567b
haykel jabri <haykel.j@gmail.com>
2021-06-15 02:27:56 +0200

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,24 +1,19 @@
 import React from 'react';
 
 import { Image } from 'antd';
-
 import 'antd/dist/antd.css';
 import './App.css';
 
 const Details = () => {
 
-
-  
     return (
         <div className="detailsContainer">
         <Image
             width={200}
             src="https:\/\/fakeimg.pl\/350x200\/?text=HellWorld"
             />
-        </div>
-        
-    );
-  
+        </div>       
+    ); 
 }
 
 export default Details;
