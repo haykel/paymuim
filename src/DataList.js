0241b681814ccba872c3c61453f7d7e18ab934a5
haykel jabri <haykel.j@gmail.com>
2021-06-15 04:40:40 +0200

    Update DataList.js

--- src/DataList.js
+++ src/DataList.js
@@ -4,53 +4,77 @@ import { PaperClipOutlined } from "@ant-design/icons";
 import Table from 'antd/lib/table';
 import 'antd/dist/antd.css';
 import './App.css';
-import data from './transactions.json'
-const DataList = () => {
-    const [dataTrans, setDataTrans] = useState([]);
-      
-    useEffect (()=>{   
-      setDataTrans(data[0].transactions);  
-    })
-    
-    const columns = [
-        {
-          title: 'DD-MM-YYYY',
-          dataIndex: 'created_at',
-          key: 'created_at',
-          sorter: {
-            compare: (a, b) => (new Date(a.created_at).valueOf()) - (new Date(b.created_at).valueOf()),
-            multiple: 1,
-          },
-          render: text => <Moment format="DD-MM-YYYY">{text}</Moment>,
-        },
-        {
-          title: 'Counterparty Name',
-          dataIndex: 'counterparty_name',
-          key: 'counterparty_name',
-        },
-        {
-          title: 'Payment type',
-          dataIndex: 'operation_type',
-          key: 'operation_type',
-        },
-        {
-          title: 'Amount',
-          dataIndex: 'amount',
-          key: 'amount',
-        },
-        {
-            title: <PaperClipOutlined />,
-            dataIndex: 'currency',
-            key: 'currency',
-            render: text => <a><PaperClipOutlined /> 1</a>,
-          },
-          
-      ];
+import data from './transactions.json';
 
-    return (
-        <Table columns={columns} dataSource={dataTrans} />
-    );
+
+const columns = [
+  {
+    title: 'DD-MM-YYYY',
+    dataIndex: 'created_at',
+    key: 'created_at',
+    sorter: {
+      compare: (a, b) => (new Date(a.created_at).valueOf()) - (new Date(b.created_at).valueOf()),
+      multiple: 1,
+    },
+    render: text => <Moment format="DD-MM-YYYY">{text}</Moment>,
+  },
+  {
+    title: 'Counterparty Name',
+    dataIndex: 'counterparty_name',
+    key: 'counterparty_name',
+  },
+  {
+    title: 'Payment type',
+    dataIndex: 'operation_type',
+    key: 'operation_type',
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    key: 'amount',
+  },
+  {
+      title: <PaperClipOutlined />,
+      dataIndex: 'currency',
+      key: 'currency',
+      render: text => <a><PaperClipOutlined /> 1</a>,
+    },
+    
+];
+
+const dataTrans = data[0].transactions;
+
+class DataList extends React.Component {
+state = {
+  selectedRowKeys: [], 
+  loading: false,
+};
+
+start = () => {
+  this.setState({ loading: true });
+  setTimeout(() => {
+    this.setState({
+      selectedRowKeys: [],
+      loading: false,
+    });
+  }, 1000);
+};
+
+onSelectChange = selectedRowKeys => {
+  this.setState({ selectedRowKeys });
+};
+
+render() {
+  const { loading, selectedRowKeys } = this.state;
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: this.onSelectChange,
+  };
   
+  return (
+      <Table rowSelection={rowSelection} columns={columns} dataSource={dataTrans} />
+  );
+}
 }
 
 export default DataList;
