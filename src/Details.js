7038360467a6f7a8cffa0a721d096d1f30dfdf38
haykel jabri <haykel.j@gmail.com>
2021-06-15 02:25:51 +0200

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Image } from 'antd';
+
+import 'antd/dist/antd.css';
+import './App.css';
+
+const Details = () => {
+
+
+  
+    return (
+        <div className="detailsContainer">
+        <Image
+            width={200}
+            src="https:\/\/fakeimg.pl\/350x200\/?text=HellWorld"
+            />
+        </div>
+        
+    );
+  
+}
+
+export default Details;
